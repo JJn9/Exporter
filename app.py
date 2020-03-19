3afa25a9875a2ec87fa3aba59a14f7396294e6c6
alex_ <alex_131410@hotmail.com>
2020-03-19 10:28:05 +0100

    fix

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"hi:o:p:",["ifile=","ofile=", "dpool="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile> -n <devicepool>')
+        print('test.py -i <inputfile> -o <outputfile> -p <devicepool>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile> -n <devicename>')
+            print('test.py -i <inputfile> -o <outputfile> -p <devicepool>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
