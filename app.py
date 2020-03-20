21f902475c48e7833bbff930d091a41b6414dc66
alex_ <alex_131410@hotmail.com>
2020-03-20 10:06:42 +0100

    all possibilites added

--- app.py
+++ app.py
@@ -7,34 +7,96 @@ import sys, getopt
 def main(argv):
     inputfile = ''
     outputfile = ''
-    deviceName = ''
+    devicePool = ''
+    css = ''
+    dirNumber = ''
+    routePartition = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:p:",["ifile=","ofile=", "dpool="])
+        opts, args = getopt.getopt(argv,"hi:o:p:c:n:r:",["ifile=","ofile=", "dpool=", "css=", "dnumber=", "route="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile> -p <devicepool>')
+        print('test.py -i <inputfile> -o <outputfile> -p <devicepool> -c <CSS> -n <directorynumber> -r <routepartition>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile> -p <devicepool>')
+            print('test.py -i <inputfile> -o <outputfile> -p <devicepool> -c <CSS> -n <directorynumber> -r <routepartition>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
         elif opt in ("-p", "--dpool"):
-            deviceName = arg
+            devicePool = arg
+        elif opt in ("-c", "--css"):
+            css = arg
+        elif opt in ("-n", "--dnumber"):
+            dirNumber = arg
+        elif opt in ("-r", "--route"):
+            routePartition = arg
 
-    read_CSV(inputfile, outputfile, deviceName)
+    read_CSV(inputfile, outputfile, devicePool, css, dirNumber, routePartition)
 
 #Cette fonction lis le csv source, le filtre selon les paramètre donnés puis enregistre le résultat dans un nouveau fichier
-def read_CSV(sourceFile, endFile, deviceName):
-    print(deviceName)
-    df = pd.concat(( [chunk[chunk['Device Pool'] == deviceName] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
-    df.filter(['Device Name','Device Type', 'Device Protocol', 'Device Pool', 'Description', 'Location', 'Media Resource Group List',
-     'User Hold MOH Audio Source', 'Network Hold MOH Audio Source', 'Device User Locale', 'Softkey Template', 'Module 1', 'Module 2', 'Phone Button Template',
-     'Owner User ID', 'Directory Number 1', 'Route Partition 1', 'Alerting Name 1', 'Display 1', 'External Phone Number Mask 1', 'Call Pickup Group 1', 'Line CSS 1', 
-     'Forward All CSS 1', 'Forward No Answer Ring Duration 1', 'Forward No Answer Internal Destination 1', 'Forward No Answer External Destination 1', 'Busy Trigger 1',
-     'Forward Busy Internal Destination 1', 'Forward Busy External Destination 1']).to_csv(endFile, index_label="id")
+def read_CSV(sourceFile, endFile, devicePool, css, dirNumber, routePartition):
+    if devicePool == '' and css == '' and dirNumber == '' and routePartition == '':
+        print('Set values')
+        
+    elif devicePool == '' and css == '' and dirNumber != '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Directory Number 1'][0:4] == dirNumber and chunk['Route Partition 1'] == routePartition] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css != '' and dirNumber == '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['CSS'] == css and chunk['Route Partition 1'] == routePartition] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css != '' and dirNumber != '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Directory Number 1'][0:4] == dirNumber and chunk['CSS'] == css] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css == '' and dirNumber == '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool and chunk['Route Partition 1'] == routePartition] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css != '' and dirNumber == '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool and chunk['CSS'] == css] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css != '' and dirNumber == '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['CSS'] == css] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css == '' and dirNumber != '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Directory Number 1'][0:4] == dirNumber] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css == '' and dirNumber == '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Route Partition 1'] == routePartition] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css == '' and dirNumber == '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css != '' and dirNumber == '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool and chunk['CSS'] == css] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css != '' and dirNumber != '' and routePartition == '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool and chunk['CSS'] == css and chunk['Directory Number 1'][0:4] == dirNumber] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool == '' and css != '' and dirNumber != '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Route Partition 1'] == routePartition and chunk['CSS'] == css and chunk['Directory Number 1'][0:4] == dirNumber] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css == '' and dirNumber != '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Route Partition 1'] == routePartition and chunk['Device Pool'] == devicePool and chunk['Directory Number 1'][0:4] == dirNumber] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css != '' and dirNumber == '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Route Partition 1'] == routePartition and chunk['CSS'] == css and chunk['CSS'] == css] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+    elif devicePool != '' and css != '' and dirNumber != '' and routePartition != '':
+        df = pd.concat(( [chunk[chunk['Device Pool'] == devicePool and chunk['CSS'] == css and chunk['Directory Number 1'][0:4] == dirNumber and chunk['Route Partition 1'] == routePartition] for chunk in pd.read_csv(sourceFile, iterator=True, chunksize=10**4)]))
+
+
+
+
+    try:
+         df.filter(['Device Name','Device Type', 'Device Protocol', 'Device Pool', 'CSS', 'Description', 'Location', 'Media Resource Group List',
+                    'User Hold MOH Audio Source', 'Network Hold MOH Audio Source', 'Device User Locale', 'Softkey Template', 'Module 1', 'Module 2', 'Phone Button Template',
+                    'Owner User ID', 'Directory Number 1', 'Route Partition 1', 'Alerting Name 1', 'Display 1', 'External Phone Number Mask 1', 'Call Pickup Group 1', 'Line CSS 1', 
+                    'Forward All CSS 1', 'Forward No Answer Ring Duration 1', 'Forward No Answer Internal Destination 1', 'Forward No Answer External Destination 1', 'Busy Trigger 1',
+                    'Forward Busy Internal Destination 1', 'Forward Busy External Destination 1']).to_csv(endFile, index_label="id")
+    except Exception as e:
+        pass
+   
 
 
 if __name__ == '__main__':
